4fedffb55f57b885b472ec5d8f9ee01c825b5c2e
584 <584@kavosh.org>
2022-01-17 11:58:40 +0330

    first upload

---
+++ train-word_based.py
@@ -0,0 +1,135 @@
+import codecs
+import pickle
+from keras.utils.np_utils import to_categorical
+import numpy as np
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Activation, Flatten, Input, Concatenate
+from keras.layers import Embedding
+from keras.layers.convolutional import Conv1D
+from keras.layers.pooling import MaxPool1D
+from keras import optimizers
+from keras import backend as K
+
+def load_data(filename):
+    data = list(codecs.open(filename, 'r', 'utf-8').readlines())
+    x, y = zip(*[d.strip().split('\t') for d in data])
+    x = np.asarray(list(x))
+    y = to_categorical(y, 2)
+
+    return x, y
+
+
+x_token_train, y_token_train = load_data('data/token_train.tsv')
+x_token_test, y_token_test = load_data('data/token_test.tsv')
+# x_morph_train, y_morph_train = load_data('data/morph_train.tsv')
+# x_morph_test, y_morph_test = load_data('data/morph_test.tsv')
+
+# print('X token train shape: {}'.format(x_token_train.shape))
+# print('X token test shape: {}'.format(x_token_test.shape))
+#
+# print('X morph train shape: {}'.format(x_morph_train.shape))
+# print('X morph test shape: {}'.format(x_morph_test.shape))
+
+from keras.preprocessing import text, sequence
+
+
+def tokenizer(x_train, x_test, vocabulary_size, char_level):
+    tokenize = text.Tokenizer(num_words=vocabulary_size,
+                              char_level=char_level,
+                              filters='')
+    tokenize.fit_on_texts(x_train)  # only fit on train
+    with open('char_saved_models/tokens.pickle', 'wb') as handle:
+        pickle.dump(tokenize, handle)
+    x_train = tokenize.texts_to_sequences(x_train)
+    # x_train2 = tokenize.texts_to_matrix(x_train)
+    x_test = tokenize.texts_to_sequences(x_test)
+
+    return x_train, x_test
+
+
+def pad(x_train, x_test, max_document_length):
+    x_train = sequence.pad_sequences(x_train, maxlen=max_document_length, padding='post', truncating='post')
+    x_test = sequence.pad_sequences(x_test, maxlen=max_document_length, padding='post', truncating='post')
+
+    return x_train, x_test
+
+
+vocabulary_size = 5000
+
+x_token_train, x_token_test = tokenizer(x_token_train, x_token_test, vocabulary_size, True)
+# x_morph_train, x_morph_test = tokenizer(x_morph_train, x_morph_test, vocabulary_size, True)
+
+max_document_length = 300
+
+x_token_train, x_token_test = pad(x_token_train, x_token_test, max_document_length)
+# x_morph_train, x_morph_test = pad(x_morph_train, x_morph_test, max_document_length)
+
+import matplotlib.pyplot as plt
+
+
+def plot_loss_and_accuracy(history):
+    fig, axs = plt.subplots(1, 2, sharex=True)
+
+    axs[0].plot(history.history['loss'])
+    axs[0].plot(history.history['val_loss'])
+    axs[0].set_title('Model Loss')
+    axs[0].legend(['Train', 'Validation'], loc='upper left')
+
+    axs[1].plot(history.history['acc'])
+    axs[1].plot(history.history['val_acc'])
+    axs[1].set_title('Model Accuracy')
+    axs[1].legend(['Train', 'Validation'], loc='upper left')
+
+    fig.tight_layout()
+    plt.show()
+
+dropout_keep_prob = 0.7
+embedding_size = 300
+batch_size = 50
+lr = 1e-4
+dev_size = 0.2
+
+num_epochs = 10
+
+# Create new TF graph
+K.clear_session()
+
+# Construct model
+convs = []
+text_input = Input(shape=(max_document_length,))
+x = Embedding(vocabulary_size, embedding_size)(text_input)
+for fsz in [2, 8]:
+    conv = Conv1D(128, fsz, padding='valid', activation='relu')(x)
+    pool = MaxPool1D()(conv)
+    convs.append(pool)
+x = Concatenate(axis=1)(convs)
+x = Flatten()(x)
+x = Dense(128, activation='relu')(x)
+x = Dropout(dropout_keep_prob)(x)
+preds = Dense(2, activation='softmax')(x)
+
+model = Model(text_input, preds)
+
+adam = optimizers.Adam(lr=lr)
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=adam,
+              metrics=['accuracy'])
+
+# Train the model
+history = model.fit(x_token_train, y_token_train,
+                    batch_size=batch_size,
+                    epochs=num_epochs,
+                    verbose=1,
+                    validation_split=dev_size)
+
+# Plot training accuracy and loss
+plot_loss_and_accuracy(history)
+
+# Evaluate the model
+scores = model.evaluate(x_token_test, y_token_test,
+                        batch_size=batch_size, verbose=1)
+print('\nAccurancy: {:.3f}'.format(scores[1]))
+
+# Save the model
+model.save('saved_model/CNN-Token-{:.3f}.h5'.format((scores[1] * 100)))
---
+++ usage-GUI.py
@@ -0,0 +1,135 @@
+import tkinter as tk
+from tkinter import filedialog, scrolledtext
+from googletrans import Translator
+from langdetect import detect
+import pickle
+from keras.preprocessing import text, sequence
+from keras.models import load_model
+
+with open('char_saved_models/tokens.pickle', 'rb') as handle:
+    tokenize = pickle.load( handle)
+
+
+def detect_lang(input_text):
+    lang = detect(input_text)
+    return lang
+
+def load_models(file_path):
+    model = load_model(file_path)
+    return model
+
+def tokenizer(x, vocabulary_size=5000, char_level=True):
+    tok = []
+    x_ = tokenize.texts_to_sequences(x)
+    for i in x_:
+        tok = tok + i
+
+    return [tok]
+
+
+def pad(x, max_document_length=300):
+    x_ = sequence.pad_sequences(x, maxlen=max_document_length, padding='post', truncating='post')
+    return x_
+detector = Translator()
+
+def eval_txt():
+
+    input_txt = txt_box.get(1.0,tk.END)
+
+    trans = detector.translate(input_txt, dest='fa')
+    translate_exp_lbl.configure(text= trans.text,  foreground="green")
+
+
+    lang = detector.detect(input_txt)
+    language_exp_lbl.configure(text= lang.lang,  foreground="green")
+
+
+    try:
+        vector = pad(tokenizer(input_txt))
+        model = load_models(model_path)
+
+        sent_pred = model.predict(vector)
+        print(sent_pred)
+        out_pred = list(sent_pred[0]).index(max(sent_pred[0]))
+        sent_exp_lbl.configure(text= out_pred,  foreground="green")
+    except:
+        sent_exp_lbl.configure(text="No Model Added", foreground="red")
+
+window = tk.Tk()
+window.title("Sentiment Analysis")
+window.geometry('800x600')
+
+translate_exp_lbl = tk.Label(window, text="", font=("Arial Bold", 16), foreground="green")
+translate_exp_lbl.pack()
+translate_exp_lbl.place(x=200, y=400)
+
+language_exp_lbl = tk.Label(window, text="", font=("Arial Bold", 18), foreground="green")
+language_exp_lbl.pack()
+language_exp_lbl.place(x=200, y=450)
+
+sent_exp_lbl = tk.Label(window, text="", font=("Arial Bold", 18), foreground="green")
+sent_exp_lbl.pack()
+sent_exp_lbl.place(x=200, y=500)
+
+
+input_model_lbl = tk.Label(window, text="Select your ML model to load:", font=("Arial Bold", 16))
+input_model_lbl.pack()
+input_model_lbl.place(x=200, y=20)
+
+output_model_lbl = tk.Label(window, text="No model added", font=("Arial Bold", 10),foreground="red")
+# output_model_lbl.grid(column=2, row=1)
+output_model_lbl.pack()
+output_model_lbl.place(x=300, y=53)
+
+
+def load_model_clicked():
+    global model_path
+
+    model_path = filedialog.askopenfilename()
+    model_name = model_path.split("/")[-1]
+    showing_name = model_name + "  successfully added."
+
+    output_model_lbl.configure(text= showing_name,  foreground="green")
+    output_model_lbl.place(x=200, y=53)
+
+add_model_btn = tk.Button(window, text="Add", command=load_model_clicked)
+add_model_btn.pack()
+add_model_btn.place(x=510, y=20)
+
+
+input_txt_lbl = tk.Label(window, text="input your text:", font=("Arial Bold", 15))
+input_txt_lbl.pack()
+input_txt_lbl.place(x=50, y=85)
+
+txt_box = scrolledtext.ScrolledText(window,width=85,height=15)
+txt_box.pack()
+txt_box.place(x=50, y=120)
+
+sentiment_lbl = tk.Label(window, text="Translate --->", font=("Arial Bold", 15))
+sentiment_lbl.pack()
+sentiment_lbl.place(x=50, y=400)
+
+
+language_lbl = tk.Label(window, text="Language --->", font=("Arial Bold", 15))
+language_lbl.pack()
+language_lbl.place(x=50, y=450)
+
+sentiment_lbl = tk.Label(window, text="Sentiment --->", font=("Arial Bold", 15))
+sentiment_lbl.pack()
+sentiment_lbl.place(x=50, y=500)
+
+
+
+
+
+
+eval_btn = tk.Button(window, text="eval", command=eval_txt,  foreground="blue", background="yellow", width=6, height=1)
+eval_btn.pack()
+eval_btn.place(x=680, y=380)
+
+
+
+window.mainloop()
+
+
+
